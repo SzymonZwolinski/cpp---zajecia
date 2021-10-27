3fb1a83e621127d0814779f1ea954072246f351b
Szymon Zwoliński <szymzwoo@gmail.com>
2021-10-27 14:17:31 +0200

    v1.2

--- linia2.cpp
+++ linia2.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
-#include "liniowa.cpp""
+#include "liniowa.cpp"
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 using namespace std;
 void liniowa2(float a, float b)
 {
 	if(a == 0)
 	{
-		cout << "brak miejsc zerowych"
+		cout << "brak miejsc zerowych";
 	}
 	else
 	{
